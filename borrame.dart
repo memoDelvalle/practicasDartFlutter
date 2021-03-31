47efb89d6fd39dd41bcc46f7a20810da5f0ee9dc
guillermo del valle <jose.guillermo.delvalle@gmail.com>
2021-03-31 16:26:49 -0600

    clases 22

--- borrame.dart
+++ borrame.dart
@@ -1,3 +1,12 @@
 void main() {
-  print('Mensaje de prueba');
+  //print('Mensaje de prueba');
+  final spiderMan = new Heroe('uno', 'dos');
+  print(spiderMan);
+}
+
+class Heroe {
+  String nombre = 'dd';
+  String poder = 'nn';
+
+  Heroe(String nombre, String poder);
 }
